e342b37ec425f8c106349b105cc97976ee1090f9
Tomas Arnay <arnaytomas@gmail.com>
2022-04-25 21:17:24 -0300

    Se agrego un mensaje nuevo

--- clases/Main.java
+++ clases/Main.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Este es un mensaje nuevo");
         String url = "jdbc:mysql://localhost:3306/gestion_contrasenias";
         String password = "riverplate20010304";
         String user = "root";
